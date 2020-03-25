4322502e16b9b575f79e6aac9b1c6fb2a9c5d358
EldarLis <eldaarliis@gmail.com>
2020-03-25 13:36:46 +0300

    Add files via upload

---
+++ lab2_7-FileArrays.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char c;
+            Console.WriteLine("f - for file; enything else for hands");
+            c = char.Parse(Console.ReadLine());
+            int N;
+            string[] str;
+            if (c == 'f')
+            {
+                StreamReader input = new StreamReader("input.ini");
+                N = int.Parse(input.ReadLine().Trim());
+                str = input.ReadLine().Trim().Split(' ');
+            }
+            else
+            {
+                Console.WriteLine("input number of values");
+                N = int.Parse(Console.ReadLine());
+                Console.WriteLine("Input all numbers of array");
+                str = Console.ReadLine().Trim().Split(' ');
+            }
+            StringBuilder res = new StringBuilder();
+            int prev = 1, curr = 1;
+            for(; curr < N; )
+            {
+                res.Append(str[curr-1] + " ");
+                curr += prev;
+                prev = curr - prev;
+            }
+
+            var path = @"output.txt";
+            string result = res.ToString();
+            File.WriteAllText(path, result);
+        }
+    }
+}
---
+++ lab2_9-String.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char c;
+            Console.WriteLine("f - for file; enything else for hands");
+            c = char.Parse(Console.ReadLine());
+            string str;
+            string[] words;
+
+            var path = @"output.txt";
+            StringBuilder res = new StringBuilder();
+
+            File.WriteAllText(path, "");
+
+            if (c == 'f')
+            {
+                StreamReader input = new StreamReader("input.ini");
+                string line = input.ReadLine();
+                while (line != null)
+                {
+                    words = line.Split(' ');
+                    for(int counter = 1; counter < words.Length; counter+= 2)
+                    {
+                        res.Append(words[counter] + " ");
+                    }
+                    File.AppendAllText(path, "\n" + res.ToString());
+                    res.Clear();
+                    line = input.ReadLine();
+                }
+            }
+            else
+            {
+                Console.WriteLine("Write lines all you want. Empty line to end");
+                string line = Console.ReadLine();
+                while (line != "")
+                {
+                    words = line.Split(' ');
+                    for (int counter = 1; counter < words.Length; counter += 2)
+                    {
+                        res.Append(words[counter] + " ");
+                    }
+                    File.AppendAllText(path, "\n" + res.ToString());
+                    res.Clear();
+                    line = Console.ReadLine();
+                }
+            }
+
+        }
+    }
+}
